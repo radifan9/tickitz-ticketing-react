2f57038c12aa4f128def6d4bb7c860480fa48a3d
radifan9 <radif.aghnadiin@gmail.com>
2025-08-01 08:54:14 +0700

    feat: added pagination to nagivate between movie pages

--- src/pages/HomeMovieList/HomeMovieList.jsx
+++ src/pages/HomeMovieList/HomeMovieList.jsx
@@ -1,21 +1,16 @@
 import React, { useEffect, useState } from "react";
-// import "./HomeMovieList.css";
+import fetchWithAuth from "../../utils/fetchWithAuth.jsx";
+import { useSearchParams } from "react-router";
 
-const NOW_PLAYING_API =
-  "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
-const GENRES_API = "https://api.themoviedb.org/3/genre/movie/list?language=en";
-const BEARER_TOKEN =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZjQzNWJiZWRhY2M0NDk3MWMxNDc1MTNkYjJmZWU2MiIsIm5iZiI6MTc1MzQ1NzY4Ni42OTQwMDAyLCJzdWIiOiI2ODgzYTQxNjQ4ZWE1NTk3MzM1NWU5NzMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.RbZg1YOqjIUcqo3C5T5GzumDBEDDi7KFi85Md91coIg";
-
-async function fetchWithAuth(url) {
-  const res = await fetch(url, {
-    headers: {
-      Authorization: `Bearer ${BEARER_TOKEN}`,
-    },
-  });
-  if (!res.ok) throw new Error("Network response was not ok");
-  return res.json();
-}
+// async function fetchWithAuth(url) {
+//   const res = await fetch(url, {
+//     headers: {
+//       Authorization: `Bearer ${BEARER_TOKEN}`,
+//     },
+//   });
+//   if (!res.ok) throw new Error("Network response was not ok");
+//   return res.json();
+// }
 
 const getGenreNameFromID = (listOfGenreID, listOfGenreName) => {
   return listOfGenreID
@@ -29,14 +24,18 @@ const getGenreNameFromID = (listOfGenreID, listOfGenreName) => {
 export default function HomeMovieList() {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [searchParams, setSearchParams] = useSearchParams({ page: 1 });
 
   useEffect(() => {
     async function getMovieList() {
+      console.log(searchParams);
       try {
-        const genresData = await fetchWithAuth(GENRES_API);
+        const genresData = await fetchWithAuth(import.meta.env.VITE_GENRES_API);
         const genresNamed = genresData.genres;
 
-        const moviesData = await fetchWithAuth(NOW_PLAYING_API);
+        const moviesData = await fetchWithAuth(
+          import.meta.env.VITE_NOW_PLAYING_API,
+        );
         const results = moviesData.results;
 
         const movieList = results.map((movie) => {
@@ -60,7 +59,7 @@ export default function HomeMovieList() {
 
   return (
     <>
-      <div className="container">
+      <div className="w-full">
         <section className="f-hero-section">
           <div className="title-list">LIST MOVIE OF THE WEEK</div>
           <div className="title-slogan">
@@ -68,7 +67,8 @@ export default function HomeMovieList() {
           </div>
         </section>
 
-        <main className="main-movie-list">
+        {/* Main */}
+        <div className="flex flex-col items-center">
           <div className="f-search-bar">
             <div className="f-search-key">
               <label htmlFor="keyword">Cari Event</label>
@@ -87,17 +87,24 @@ export default function HomeMovieList() {
           </div>
 
           {/* Grid Movie List */}
-          <div className="g-movie-list">
+          <div className="grid grid-cols-2 md:grid-cols-4">
             {loading ? (
               <div>Loading...</div>
             ) : (
               movies.map((movie) => (
                 <div className="f-singular-movie" key={movie.id}>
                   <div className="container-image-poster">
-                    <img src={movie.src} alt={movie.title} />
+                    <img
+                      width="264px"
+                      src={movie.src}
+                      alt={movie.title}
+                      onClick={(e) => {
+                        e.preventDefault();
+                      }}
+                    />
                     <div className="hover-link">
-                      <a href="../pages/details.html">Details</a>
-                      <a href="../pages/details.html">Buy Ticket</a>
+                      {/* <a href="../pages/details.html">Details</a> */}
+                      {/* <a href="../pages/details.html">Buy Ticket</a> */}
                     </div>
                   </div>
                   <h3>{movie.title}</h3>
@@ -114,18 +121,33 @@ export default function HomeMovieList() {
           </div>
 
           {/* <!-- Page Navigation --> */}
-          <div className="container-page-nav">
-            <button className="btn-blue">1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
+          <div className="flex gap-2">
+            {[1, 2, 3].map((pageNumber, idx) => {
+              return (
+                <span
+                  key={idx}
+                  className="flex h-5 w-5 cursor-pointer items-center justify-center rounded-full bg-[#2563EB] p-5 text-[#FFFFFF]"
+                  onClick={() => {
+                    // Logika paginasi
+                    setSearchParams((searchParams) => {
+                      if (searchParams.has("page")) {
+                        searchParams.set("page", pageNumber);
+                      } else {
+                        searchParams.append("page", pageNumber);
+                      }
+                      return searchParams;
+                    });
+                  }}
+                >
+                  {pageNumber}
+                </span>
+              );
+            })}
             <button className="btn-blue btn-arrow">
               <img src="../assets/icons/white-right-arrow.png" alt="" />
             </button>
           </div>
-
-          
-        </main>
+        </div>
       </div>
     </>
   );
